4e7aca20ab781520e985fe25678141cd5ef5aa9a
ravzzy <swiftwindwalker2@gmail.com>
2025-02-24 23:18:47 +0530

    iframe fix attempt

--- js/homepage.js
+++ js/homepage.js
@@ -1091,15 +1091,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener("resize", adjustOverlayPosition);
 });
-
-
-document.addEventListener("touchmove", function(event) {
-    if (event.touches.length > 1 || event.scale !== 1) { 
-        event.preventDefault(); 
-    }
-}, { passive: false });
-
-document.addEventListener("gesturestart", function(event) {
-    event.preventDefault();
-});
-
